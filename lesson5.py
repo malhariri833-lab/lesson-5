54ca8691adecfa8475a57050749ff50ba1a31a75
mohamad1 <malhariri833@gmail.com>
2026-09-09 21:16:32 +0200

    inntel

---
+++ lesson5.py
@@ -0,0 +1,14 @@
+tempreture=int(input("what is the tempreture "))
+if tempreture < 10:
+    print("were a jackt")
+    print("its very cold outside")
+else:
+    print("where a t-shirt")
+    print("its getting warmer")
+
+
+age=int(input ("how old are you ") )
+if age < 18:
+    print("you are too yong for this football club")
+else:
+      print("you may come and vist our football club")
