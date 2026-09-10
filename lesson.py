58f5d099e7c6cbb4510d7553a3af04abe6f0d088
mohamad1 <malhariri833@gmail.com>
2026-09-10 21:23:50 +0200

    5

---
+++ lesson.py
@@ -0,0 +1,31 @@
+score=int(input("inter your score "))
+if score>=9:
+    print("A grade")
+elif score>=8:
+    print("B grade")
+elif score>7:
+    print("C grade")
+else:
+    print("F grade")
+
+
+
+
+if score>=9 and score<=10:
+    print("A grade")
+elif score>=8 and score<9:
+    print("B grade")
+elif score>7:
+    print("C grade")
+else:
+    print("F grade")
+
+
+
+name=input("inter your name ")
+if name==("mohamad") or name=="adam":
+    print("good boys")
+else:
+    print("bad boys")
+
+ 
